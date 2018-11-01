c0737de72d0a5d898642345f26e6a2ee436b0f9c
Zan Javed <zjaved101@gmail.com>
2018-11-01 00:34:25 -0700

    added 3 seconds to wait and cleaned up code

--- commands/rnick.js
+++ commands/rnick.js
@@ -1,22 +1,4 @@
 exports.run = (client, message, args) => {
-    
-    // const wordSite = "http://svnweb.freebsd.org/csrg/share/dict/words?view=co&content-type=text/plain";
-    // const request = require('request');
-    // const timeout = 10 * 1000;
-    // const opts = {url: url, timeout: timeout};
-
-    // requests(opts, function (err, res, body) {
-    //     if(err) {
-    //         console.dir(err)
-    //         return
-    //     }
-
-    //     const statusCode = res.statusCode;
-    //     console.log('status code: ' + statusCode);
-    // });
-
-    // counter = 0;
-    // run = 90;
 
     function changeNick(text, member) {
         member.setNickname(text[Math.floor(Math.random() * text.length)]).catch("ERROR:" + console.error);
@@ -24,14 +6,11 @@ exports.run = (client, message, args) => {
 
     //This is how you can change a users nickname
     const request = require('request');
-    // const wordSite = "http://svnweb.freebsd.org/csrg/share/dict/words?view=co&content-type=text/plain"
     const wordSites = ["http://svnweb.freebsd.org/csrg/share/dict/words?view=co&content-type=text/plain", "https://www.cs.cmu.edu/~biglou/resources/bad-words.txt"];
     
     let index = 0;
-    if(args.length > 1){
+    if(args.length > 1)
         index = args[1];
-        // console.log(index);
-    }
 
     request({
         url: wordSites[index],
@@ -40,31 +19,16 @@ exports.run = (client, message, args) => {
         if(!err && res.statusCode === 200) {
             let member = message.mentions.members.first();
             let parsed = body.split('\n');
-            // console.log(parse);
+
             const count = Math.floor(Math.random() * 50);
-            for(let i = 0; i < count; i++) {
-                // member.setNickname(parse[Math.floor(Math.random() * parse.length)]).catch(console.error);
-                setTimeout(changeNick, 2000, parsed, member);
-                // console.log(body);
-                // let parsed = JSON.parse(body);
-                // console.log(parsed);
-                // console.log(body.key);
-                // console.log(body[Math.floor(Math.random() * body.length)]);
-                // member.setNickname(body[Math.floor(Math.random() * body.length)]);
-            }
+            for(let i = 0; i < count; i++) 
+                setTimeout(changeNick, 3000, parsed, member);
+            
             console.log('finished changing nickname');
-            // console.log(body[Math.floor(Math.random() * body.length)]);
-            // member.setNickname(body[Math.floor(Math.random() * body.length)]);
         }
         else {
             console.log(err);
         }
     });
-    
-    // var member = message.mentions.members.first();
-    // member.setNickname("prebbbb");
-
-
-
     
 }
