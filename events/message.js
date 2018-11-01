a3c950d7df600bda1e87cb274b71e1710bd12db6
Zan Javed <zjaved101@gmail.com>
2018-10-31 22:44:52 -0700

    added message event

---
+++ events/message.js
@@ -0,0 +1,20 @@
+module.exports = (client, message) => {
+    // ignore all bots
+    if(message.author.bot) return;
+
+    // ignore messages not starting with the prefix (in config.json)
+    if(message.content.indexOf(client.config.prefix) !==0 ) return;
+
+    // our standard argument/command name definition
+    const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    // grab the command data from the client.commands Enmap
+    const cmd = client.commands.get(command);
+
+    // if command doesn't exist, silently exit and do nothing
+    if(!cmd) return;
+
+    // run command
+    cmd.run(client, message, args);
+};
